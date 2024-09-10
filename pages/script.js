d2a874cf2e715cd1f8ff2af7463ddcafbdb6c19a
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2024-09-10 12:30:09 +0000

    change image and make the js auto template ready

--- pages/script.js
+++ pages/script.js
@@ -42,7 +42,95 @@ function previous() {
 }
 
 
+// product list
 
 
+let productList = [
+    {
+        id : '1',
+        Image : '../assets/images/product1.jpg',
+        title : 'products 1',
+        description : 'this is product 1' 
+    },
+    {
+        id : '2',
+        Image : '../assets/images/product2.jpg',
+        title : 'products 2',
+        description : 'this is product 2'
+    },
+    {
+        id : '3',
+        Image : '../assets/images/product3.jpg',
+        title : 'products 3',
+        description : 'this is product 3'
+    },
+    {
+        id : '4',
+        Image : '../assets/images/product4.jpg',
+        title : 'products 4',
+        description : 'this is product 4'
+    },
+    {
+        id : '5',
+        Image : '../assets/images/product5.jpg',
+        title : 'products 5',
+        description : 'this is product 5'
+    }
+];
+
+let products = document.getElementById('products');
+
+
+productList.forEach(product => {
+    
+
+    // create the card div and append as a child the product cards div
+
+    let card = document.createElement('div');
+    card.className = 'card'
+    products.appendChild(card);
+
+
+// first part of the card div, card image
+    // createa car-image div and append as a child the card div
+
+    let cardImage = document.createElement('div');
+    cardImage.className = 'card-image';
+    card.appendChild(cardImage);
+
+
+    // create a image and append as a child of card image
+
+    let image = document.createElement('img');
+    image.src = product.Image;
+    cardImage.appendChild(image);
+
+
+
+
+
+
+
+
+// second part of the card div, card content
+    // create a card content div and append as a child of card
+
+
+
+    // create a h2 element and append as child of card content
+
+
+
+    // create a p element and append as of card content
+
+
+
+    // create a button element and append as child of card content
+
+
+
+
+
+});
 
 
